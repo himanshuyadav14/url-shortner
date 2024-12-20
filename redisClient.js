d6cbfbcedd5141d73f9d7dc9226943b22133d8cf
himanshuyadav14 <Himanshu1156044@gmail.com>
2024-12-21 01:32:38 +0530

    final change

--- redisClient.js
+++ redisClient.js
@@ -1,7 +1,7 @@
 const redis = require("redis");
 
 const redisClient = redis.createClient({
-  url: process.env.REDIS_URL || "redis://127.0.0.1:6379",
+  url: process.env.REDIS_URI,
 });
 
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
